f790a6ba1b5864189ad39bfa71215fea55268271
PabloRestrepo01 <70071301+PabloRestrepo01@users.noreply.github.com>
2022-10-26 21:14:39 -0500

    a

--- pregunta_11.sql
+++ pregunta_11.sql
@@ -38,6 +38,6 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 -- 
-SELECT SUBSTR(c14, 1, 4), COUNT(*) FROM tbl1
+SELECT SUBSTR(c14, 1, 4) AS `strftime('%Y', c14)`, COUNT(*) FROM tbl1
 WHERE SUBSTR(c14, 1, 4) = 2018
 GROUP BY SUBSTR(c14, 1, 4);
